c6d46bad1360762cfc79b64aa7c3fadedd02e78f
DrBenSmith <bensmith1@msn.com>
2020-12-30 17:44:41 +0000

    File structure updated

--- scripts/path.R
+++ scripts/path.R
@@ -4,9 +4,9 @@ incident_report_file_path_062020_to_092020 <- "inputs/incident_report/NPM SM Inc
 incident_report_file_path_jan_2020_to_nov_2020 <- "inputs/incident_report/SMSD Incident database 2020 Jan - Nov.csv"
 
 # Rainfall data: this is collected from external websites. Code exists for this:
-chirps_dataset_path <- "inputs/chirps_daily_rainfall_2020_jan_through_august.csv" 
-cfsr_dataset_path <- "inputs/cfsr_daily_rainfall_2020_jan_through_october_16.csv"
+chirps_dataset_path <- "inputs/Processed_precipitation_data/chirps_daily_rainfall_2020_jan_through_august.csv" 
+cfsr_dataset_path <- "inputs/Processed_precipitation_data/cfsr_daily_rainfall_2020_jan_through_october_16.csv"
 
 # Folder of onthly precipitation csv's. Data from UNDP for 5 gauges:
 # GSB Cox's Bazaar, GSB Teknaf, UN Camp 16, UN Kuturc & UN Chakmarkul
-precipitation_folder_path <- "inputs/Processed_precipitation_data/2020/"
+precipitation_folder_path <- "inputs/Processed_precipitation_data/UNDP_2020/"
