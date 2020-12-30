4c6d568e39a01a318a146b9584f252ceba54b96d
DrBenSmith <bensmith1@msn.com>
2020-12-30 17:32:21 +0000

    Updated comments.

--- scripts/path.R
+++ scripts/path.R
@@ -1,8 +1,12 @@
+# Incident Data - this is provided by IOM NPM:
 incident_report_file_path_04_2019_to_062020 <- "inputs/incident_report/NPM SM Inc- 20190421 To 20200606-W.csv"
 incident_report_file_path_062020_to_092020 <- "inputs/incident_report/NPM SM Inc- 20200601 To 20200930.csv"
 incident_report_file_path_jan_2020_to_nov_2020 <- "inputs/incident_report/SMSD Incident database 2020 Jan - Nov.csv"
 
+# Rainfall data: this is collected from external websites. Code exists for this:
 chirps_dataset_path <- "inputs/chirps_daily_rainfall_2020_jan_through_august.csv" 
 cfsr_dataset_path <- "inputs/cfsr_daily_rainfall_2020_jan_through_october_16.csv"
 
+# Folder of onthly precipitation csv's. Data from UNDP for 5 gauges:
+# GSB Cox's Bazaar, GSB Teknaf, UN Camp 16, UN Kuturc & UN Chakmarkul
 precipitation_folder_path <- "inputs/precipitation_data/2020/"
