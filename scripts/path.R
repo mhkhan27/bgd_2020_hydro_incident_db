040c915779fb2aed7b28fe13ce0c08241f5cde53
DrBenSmith <bensmith1@msn.com>
2020-12-30 17:30:39 +0000

    Paths updated with some background information

--- scripts/path.R
+++ scripts/path.R
@@ -1,5 +1,7 @@
-incident_report_file_path_062020_to_092020 <- "inputs/incident_report/NPM SM Inc- 20200601 To 20200930.csv"
 incident_report_file_path_04_2019_to_062020 <- "inputs/incident_report/NPM SM Inc- 20190421 To 20200606-W.csv"
+incident_report_file_path_062020_to_092020 <- "inputs/incident_report/NPM SM Inc- 20200601 To 20200930.csv"
+incident_report_file_path_jan_2020_to_nov_2020 <- "inputs/incident_report/SMSD Incident database 2020 Jan - Nov.csv"
+
 chirps_dataset_path <- "inputs/chirps_daily_rainfall_2020_jan_through_august.csv" 
 cfsr_dataset_path <- "inputs/cfsr_daily_rainfall_2020_jan_through_october_16.csv"
 
